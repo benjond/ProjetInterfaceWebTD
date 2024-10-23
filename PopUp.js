6d40e5bc6385d0af1fbefce0ab6c71ccbc01f696
Abid-FontaineLiam <liam.abid--fontaine@edu.univ-fcomte.fr>
2024-10-23 20:47:11 +0200

    Changement Page Boutique
    Changement de boutique.html, PopUp.js, styles.css.
    Avec ajout de BackGroundSiteModern.png

--- PopUp.js
+++ PopUp.js
@@ -17,3 +17,150 @@ document.querySelectorAll('#ClosePopup').forEach(button => {
         document.getElementById('BarNavigation').style.filter = "none";
     });
 });
+
+// LeagueOfLegendsPopUp :
+document.querySelectorAll('#OpenPopupLeagueOfLegends').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('LeagueOfLegendsPopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupLeagueOfLegends').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('LeagueOfLegendsPopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
+
+// FortnitePopUp :
+document.querySelectorAll('#OpenPopupFortnite').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('FortnitePopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupFortnite').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('FortnitePopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
+
+// Destiny2PopUp :
+document.querySelectorAll('#OpenPopupDestiny2').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('Destiny2PopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupDestiny2').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('Destiny2PopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
+
+// MinecraftPopUp :
+document.querySelectorAll('#OpenPopupMinecraft').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('MinecraftPopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupMinecraft').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('MinecraftPopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
+
+// ValorantPopUp :
+document.querySelectorAll('#OpenPopupValorant').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('ValorantPopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupValorant').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('ValorantPopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
+
+// CSGOPopUp :
+document.querySelectorAll('#OpenPopupCSGO').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('CSGOPopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupCSGO').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('CSGOPopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
+
+// RocketLeaguePopUp :
+document.querySelectorAll('#OpenPopupRocketLeague').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('RocketLeaguePopUp').style.display = 'block';
+        document.getElementById('Effet').style.display = 'block';
+        document.getElementById('BackGround').style.filter = "blur(10px)";
+        document.getElementById('BoutiqueMain').style.filter = "blur(10px)";
+        document.getElementById('BarNavigation').style.filter = "blur(10px)";
+    });
+});
+
+document.querySelectorAll('#ClosePopupRocketLeague').forEach(button => {
+    button.addEventListener('click', function() {
+        document.getElementById('RocketLeaguePopUp').style.display = 'none';
+        document.getElementById('Effet').style.display = 'none';
+        document.getElementById('BackGround').style.filter = "none";
+        document.getElementById('BoutiqueMain').style.filter = "none";
+        document.getElementById('BarNavigation').style.filter = "none";
+    });
+});
